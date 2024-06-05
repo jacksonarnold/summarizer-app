e1dcd42c0ad18b515e9e5d9533a5b4c0a6370c48
Jackson Arnold <jacksonarnold96@gmail.com>
2024-06-05 18:41:52 -0500

    fix service worker bundling

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,31 @@
 import React from "react";
 import { FileUpload } from "@/components/file-upload";
 import { getUser } from "@/lib/mongo";
+import {initializeApp} from "firebase/app";
+import {getAuth, getIdToken} from "firebase/auth";
+import {firebaseConfig} from "@/lib/firebase/config";
 
 export default async function Page() {
-    // add this below
-    // let response = await fetch("http://localhost:8000/test");
-    // let data = await response.json();
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    await auth.authStateReady();
 
-    let user = await getUser("");
+    if (auth.currentUser) {
+        let authToken = await getIdToken(auth.currentUser);
 
+        // add this below
+        const headers = new Headers();
+        headers.append("Authorization", "Bearer " + authToken);
+        let response = await fetch("http://localhost:8000/test", {
+                headers: headers
+            }
+        );
+
+        let data = await response.json();
+    }
+
+    // let user = await getUser("");
+    let user = "";
     return (
         <section>
             <h1 className="text-3xl font-bold mb-10 text-center">
